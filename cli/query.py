56dcc6b09418b5179e33cf3b431537a1563d3bcc
irene.psomiadou <eirini.psomiadou@deliveryhero.com>
2021-10-02 17:31:39 +0200

    now user can place orders

--- cli/query.py
+++ cli/query.py
@@ -16,22 +16,30 @@ else:
 
 print("Greetings", user_name)
 
+#start of main loop
 while(repeat):
     print("How can I be of service?")
 
-    action= int(input("1. List items by Warehouse?\n2. Search an item and place an order?\n3. Quit  "))
+    action= int(input("1. List items by Warehouse?\n2. Search an item and place an order?\n3. Quit\n---Type the number of the operation:  "))
+#start of action1
     if action==1:
-     for i in warehouse1 and warehouse2:
-         print(i)
+     print("Warehouse1:")
+     for i in warehouse1 :
+         print(i,)
          print("-----------------------------------")
-
+     print("warehouse2")
+     for i in warehouse2:
+         print("- ",i)
+#end of action 1
+#start of action 2
     if action==2:
         item_name= (input("What is the name of the item?"))
         elm_count_1=warehouse1.count(item_name)
         elm_count_2=warehouse2.count(item_name)
-
+#printing availability
         if item_name in warehouse1 and warehouse2:
            print("Location: Both Warehouses")
+           print("Amount Available:",(elm_count_1+elm_count_2))
            if elm_count_1>elm_count_2:
             print ("Maximum availability:",(elm_count_1),"in Warehouse 1")
            elif elm_count_1<elm_count_2:
@@ -47,11 +55,32 @@ while(repeat):
            print("Location: Warehouse2")
         else:
            print(item_name,"is not in stock")
+           continue
+#end of printing availability
+#start of placind order
 
-    # Else, if they pick 3
+        print("Would you like to place an order for this item?")
+        order_action = int(input("1.Yes \n2.No\n: ---Type the number of the operation:  "))
+        if order_action == 1:
+           item_number = int(input("How many items would you like? "))
+#checking if we have enough items
+           if item_number <= (elm_count_1 + elm_count_2):#if yes
+             print("The order has been placed",item_number)#make order
+           else:#if not
+            print("There are not this many items available.The maximum amount that can be ordered is ",(elm_count_1 + elm_count_2))
+            max_order=int(input("Would you like to order the maximum amount available?,\n1.Yes\n2.No\n:---Type the number of the operation:  "))
+            if max_order==1:
+              print("An order of",(elm_count_1 + elm_count_2),item_name,"has been placed")
+
+#rnd of placing order
+#end of action2
+#start of action3
     if action==3:
         repeat = False
         print("Safe travels,",user_name,"!")
+    #else:
+        #print("type a valid operator!")
 
-#that's all for today!
-# I lied :(
+
+#that's all folks!
+
